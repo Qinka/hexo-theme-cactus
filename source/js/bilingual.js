e5622948815013790af2201f82089a8f1e1bfa24
Johann Lee <me@qinka.pro>
2020-03-25 20:11:14 +0800

    Update for bilingual

--- source/js/bilingual.js
+++ source/js/bilingual.js
@@ -1,7 +1,10 @@
-var leftParagraphs = $('.content-bil .bil-first-column').find('p, h1, h2, h3, h4, h5, h6');
-var rightParagraphs = $('.content-bil .bil-second-column').find('p, h1, h2, h3, h4, h5, h6');
+var left = $('#the-l-column');
+var leftParagraphs = left.find('p, h1, h2, h3, h4, h5, h6');
+var right = $('#the-r-column')
+var rightParagraphs = right.find('p, h1, h2, h3, h4, h5, h6');
+var col_state = null;
 
-var bil_align = () => {
+const bil_align = () => {
     /* 如果页面宽度大于 800px，进行段落对齐，见下一节 */
     if(window.matchMedia('screen and (min-width: 480px)').matches) {
         leftParagraphs.each((i, thiz) => {
@@ -19,6 +22,47 @@ var bil_align = () => {
     } else {
         leftParagraphs.removeAttr('style'), rightParagraphs.removeAttr('style');
     }
+};
+
+const column_hidden = (i) => {
+    if (i != undefined) {
+        col_state += i;
+        col_state = Math.max(-1, col_state);
+        col_state = Math.min( 1, col_state);
+    }
+
+    if (col_state < 0) {
+        left.hide();
+        right.show();
+        right.width('auto');
+        rightParagraphs.each((i, thiz) => {
+            $(thiz).height('auto');
+        });
+    } else if (col_state > 0) {
+        left.show();
+        right.hide();
+        left.width('auto');
+        leftParagraphs.each((i, thiz) => {
+            $(thiz).height('auto');
+        });
+    } else {
+        left.show();
+        right.show();
+        left.width('');
+        right.width('');
+        bil_align();
+    }
+};
+
+const when_ready = () => {
+    if (col_state == null) {
+        if (document.location.search == '?left') {
+            col_state = 1;
+        } else if (document.location.search == '?right') {
+            col_state = -1;
+        }
+    }
+    column_hidden();
 }
 
 var resizeHandler = 0;
@@ -27,17 +71,17 @@ $(window).resize(() => {
     if(resizeHandler) {
         clearTimeout(resizeHandler);
     }
-    resizeHandler = setTimeout(bil_align, 500);
+    resizeHandler = setTimeout(when_ready, 500);
 });
 
 if($('.content-bil img').length) {
-    $('.content-bil img').ready(bil_align);
+    $('.content-bil img').ready(when_ready);
 } else {
-   $(bil_align);
+   $(when_ready);
 }
 
 if(document.fonts) {
     document.fonts.ready.then(function () {
-        bil_align();
+        when_ready();
     });
 }
